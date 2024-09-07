552f25e26032439c7bdffc613472fc91ba91f374
Mahender Kumar <mkayrajput8@gmail.com>
2024-09-08 00:03:35 +0530

    First day leaning - frontend masters

---
+++ ligma.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    dsa: [
+        "InsertionSort",
+        "MergeSort",
+        "Queue",
+        "Stack",
+        "QuickSort",
+        "DijkstraList",
+        "PrimsList",
+    ],
+}
